69f8424b30389a5ce8cb1271d72c44d79c835586
K6D <122948598+K-6-D@users.noreply.github.com>
2023-10-30 16:59:07 -0700

    pit

--- mod-installer.sh
+++ mod-installer.sh
@@ -9,7 +9,7 @@ mod_name=(
 	'Enduro-Loop'
 	'Enduro-Bike-Pack'
 	'OEM-Bike-Pack'
-	'PITBIKE_HONDA'
+	'PITBIKE_1'
 #	'K6D-Profile'
 ) 
 mod_dir=(
@@ -20,7 +20,7 @@ mod_dir=(
 	'/mods'
 	'/mods'
 	'/mods'
-	'/mods/bikes'
+	'/mods'
 #	'/profiles'
 ) 
 mod_links=(
@@ -31,7 +31,7 @@ mod_links=(
 	"https://v0opyg.sn.files.1drv.com/y4myAmct8BvddK3R8JxrcMrqdfP3Gwhpya75yZE1bLdxlpdShJ4fRta2oC-wwIcTTsNglaJnEar0EcG_4aKtjrV8iFzImeQrkL_DT_NTb7-ee3qWxGJG6nbkxxNXBBrVkT2wd-wFO4InYTKS0c_KmwR7Ac_pj6gH7YKs4Z9qV8CYX_onUSxFqLnpkpOKYD6HMx_/Verified-G0-V1-EnduroLoop-Main.zip?download&psid=1"
 	"https://xortjw.sn.files.1drv.com/y4mWzd45vopRDYI6m8dv1fn02wOCuV_jiDAJXORjyTMLqBpPFkOMPGsGFheOiN0uMCBAJsrrgM9z-k2aLUtaEu4PPI60a_bVGevsG52Fot8NRApflTjmt0nNlh6X9sAPrA2iEiUY9ncqVz_mn0hhQDFNqnhLiI_TJnIeT9x_abn_X5hSc6kOEZMV4WcrN8skDPW/Verified-G0-V1-Enduro-Bike-Pack-v0.15.4.zip?download&psid=1"
 	"https://download2439.mediafire.com/t18oop8i0wdgYAONk43QuAPXo47QV735CVTf2VwriicPhzwjfpHqn_QWTRUgYVy7_5ZLMnEih2Y2v8OAyp3tvUxmH5m6df_NrQkr2mlMKsetaZaydeUd0bqB9XPbb3Fm9snQf8l7FQRIr8j04z4v3ZTl4JC1metk0owfAml4JmLq/o029owuf76gbqrs/MX+OEM+v0.18.2.zip"
-	"https://mxb-mods.com/wp-content/uploads/2022/12/honda-2022-pnt.pnt"
+	"https://87dmbq.sn.files.1drv.com/y4mRgeSfY59QsabxX0UuJztNJDF4kjNkKtAmMs1GEmPCi2NR3A9BDNz3myxZm4MNBhwpgQqRbPofVLX3bme6sU2KXJPMRsaE2jrbuMNgb6y5Vepqpalkpe7OiK8m_uzBcf9YuwHL-2aNlh6wTyGMC5nHG35VtX-rzh8A06C3_g5BoWoFDYJz2RkmTm3augUFxMM/pit_bikes.zip?download&psid=1"
 #	"https://www.dropbox.com/scl/fi/px0x5skd1q0e3gyt8txie/K6D-Profile.zip?rlkey=79m1y45ilym5ujw4wv8ss32q6&dl=1"
 )
 
